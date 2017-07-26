606ddba152a7ff2c08ba345255d489b9baf30975
atan009 <atan009@users.noreply.github.com>
2017-07-26 10:01:54 -0400

    Player moves

--- js/app.js
+++ js/app.js
@@ -5,6 +5,9 @@ var Enemy = function() {
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
+    this.x = 0;
+    this.y = 62.25;
+    this.mv = 1;
     this.sprite = 'images/enemy-bug.png';
 };
 
@@ -14,6 +17,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.mv = this.mv * dt;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,13 +28,39 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var player = function () {
+    this.x = 202;
+    this.y = 373.5;
+    this.mv = 1;
+    this.sprite = 'images/char-boy.png';
+};
 
+player.prototype.update = function() {
+    this.mv = this.mv;
+};
 
+player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+player.prototype.handleInput = function(e) {
+    if (e === 'left') {
+        this.x = this.x - 101;
+    } else if (e === 'right') {
+        this.x = this.x + 101;
+    } else if (e ==='up') {
+        this.y = this.y - 83;
+    } else if (e === 'down') {
+        this.y = this.y + 83;
+    }
+}
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-
+var allEnemies = [];
+allEnemies.push(new Enemy());
+var player = new player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
